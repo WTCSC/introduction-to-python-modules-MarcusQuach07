6a5b621098c45d9943f8330c58ca9c4991306cbd
sh09unn3r <marcusquach07@gmail.com>
2024-09-12 13:26:18 -0600

    Changed for loop so it doesnt close in the loop and will also return avg

--- average.py
+++ average.py
@@ -1,5 +1,6 @@
 import text_utils
 # Defines average
+
 def average():
     # Creates a variable named file to open filename in read mode
     file = open('sample.txt', 'r')
@@ -8,17 +9,20 @@ def average():
     # Sets line_count and word_count to zero for possible array and list
     line_count = 0
     word_count = 0
+
     #Creates a for loop for line in lines
     for line in lines:
         # Creates an array to go up every line
         line_count = line_count + 1
         # Makes word_count into a variable and adds word_count into the text_utils to count all the words in each line
-        word_count = word_count + text_utils.count_words(lines)
-        # Closes the file
-        file.close()
-        # Before average constant equation(divides word count by line count to find the average words per line)
-        b4avg = word_count // line_count
-        #Returns variable
-        return(b4avg)
-    #Prints line for averages words per line
+        word_count = word_count + text_utils.count_words(line)
+
+    # Closes the file
+    file.close()
+    # average equation(divides word count by line count to find the average words per line)
+    avg = word_count // line_count
+    #Returns variable
+    return(avg)
+
+#Prints line for average words per line
 print(f"Average words per line: {average()}")
