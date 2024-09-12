70bc7f80877c28568cd16aff2d78903e4fec49c3
sh09unn3r <marcusquach07@gmail.com>
2024-09-12 13:08:59 -0600

    Done

--- average.py
+++ average.py
@@ -1,12 +1,24 @@
-"""
-Import the text_utils module you created and calculate the average number of
-words per line in a given text file. The text file that will be used to test
-this is the text file called "sample.txt" (located in the same directory as
-this exercise). The average number of words per line should be rounded down to
-the nearest integer.
-
-Print the average number of words per line in the text file in the following
-format:
-
-"Average words per line: [average]"
-"""
+import text_utils
+# Defines average
+def average():
+    # Creates a variable named file to open filename in read mode
+    file = with open(filename, 'r'):
+        # Creates a variable named lines to read the lines in the file
+        lines = file.readlines()
+        # Sets line_count and word_count to zero for possible array and list
+        line_count = 0
+        word_count = 0
+        #Creates a for loop for line in lines
+        for line in lines:
+            # Creates an array to go up every line
+            line_count = line_count + 1
+            # Makes word_count into a variable and adds word_count into the text_utils to count all the words in each line
+            word_count = word_count + text_utils.count_words(lines)
+            # Closes the file
+        file.close()
+        # Before average constant equation(divides word count by line count to find the average words per line)
+        b4avg = word_count // line_count
+        #Returns variable
+        return(b4avg)
+    #Prints line for averages words per line
+    print(f"Average words per line: {average()}")
--- text_utils.py
+++ text_utils.py
@@ -1,14 +1,24 @@
+# Define count_chars as a function to put text into as a list to count the amount of characters
 def count_chars(text):
-    """
-    Count the number of characters in a text
-    """
+    # Returns the length of the text
+    return len(text)
 
+# Define count_words as a function to put text into a list to count the amount of words
 def count_words(text):
-    """
-    Count the number of words in a text
-    """
+    # splits the text up into individual words as a variable named word_count
+    word_count = text.split()
+    # return the length of word_count
+    return len(word_count)
 
+# Define count_sentences as a function to put punctuation into as a list to count the amount of sentences
 def count_sentences(text):
-    """
-    Count the number of sentences in a text
-    """
+    # Counts the amount of periods
+    period_count = text.count('.')
+    # Counts the amount of exclamation marks
+    exclamation_count = text.count('!')
+    # Counts the amount of question marks
+    question_mark_count = text.count('?')
+    # Adds all the counts up to a total amount of sentences
+    total_sentence = (period_count + exclamation_count + question_mark_count)
+    # Returns the total sentence
+    return(total_sentence)
